1182d07aba7bfffcf3037cb5e9b7960b4f11ce75
Aakil Fernandes <aakilfernandes@gmail.com>
2015-05-16 13:31:06 -0400

    change demo ip

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -2,6 +2,6 @@ yes | tugboat rebuild anonymart -k=11861819
 sleep 10
 tugboat wait anonymart --state active
 sleep 10
-ssh-keygen -R 104.236.98.89
+ssh-keygen -R 45.55.192.76
 sleep 10
-ssh root@104.236.98.89 -i ~/.ssh/digitalocean 'bash -s' < bin/ssh.sh
+ssh root@45.55.192.76 -i ~/.ssh/digitalocean 'bash -s' < bin/ssh.sh
