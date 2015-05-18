b2c45e8ddb98fb40f5b74dee875d31eed873a372
Aakil Fernandes <aakilfernandes@gmail.com>
2015-05-18 12:43:01 -0400

    add cron job for clearing old jobs

--- app/models/Job.php
+++ app/models/Job.php
@@ -3,6 +3,7 @@
 class Job extends \Eloquent {
 	protected $fillable = ['name'];
 	protected $dates = ['completed_at'];
+	const MAXIMUM_JOBS = 100;
 
 	public function getDurationAttribute(){
 		return $this->completed_at->diffInSeconds($this->created_at);
@@ -21,6 +22,17 @@ class Job extends \Eloquent {
 	}
 
 	public static function clearOldJobs(){
+		$names = self::getUniqueNames();
+
+		foreach($names as $name){
+			$jobsCount = Job::where('name','=',$name)->count();
+			if($jobsCount <= self::MAXIMUM_JOBS) continue;
+
+			$name = mysqli::real_escape_string($name);
+			$limit = (int) $jobsCount-self::MAXIMUM_JOBS;
+
+			DB::delete("DELETE FROM jobs WHERE name = '{$name}' ORDER BY created_at ASC LIMIT $limit");
+		}
 
 	}
 
