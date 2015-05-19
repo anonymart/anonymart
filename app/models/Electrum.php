0fcbd2b48273b7d3030d57dc0acd4a52ec30bfe3
Aakil Fernandes <aakilfernandes@gmail.com>
2015-05-19 18:11:12 -0400

    restore path

--- app/models/Electrum.php
+++ app/models/Electrum.php
@@ -10,6 +10,7 @@ class Electrum{
 	}
 	
 	public function exec($command,$inputs = null,$usesExistingWallet = true){
+        $command = "{$this->path} $command";
 
         if($usesExistingWallet)
         	$command.="  --wallet={$this->walletPath}";
@@ -20,7 +21,7 @@ class Electrum{
         if($inputs !== null)
         	$command = 'echo -ne \''.implode('\n',$inputs).'\n\' | '.$command;
 
-        var_dump($command);
+        var_dump($command)
 
         $resultLines = [];
 		$lastResultLine = exec($command,$resultLines);
