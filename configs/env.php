0bd8852b7fd6cf00f4c6f248cf993e2e688ba236
Aakil Fernandes <aakilfernandes@gmail.com>
2015-05-19 18:13:26 -0400

    use electrum in local/bin

--- configs/env.php
+++ configs/env.php
@@ -1,6 +1,6 @@
 <?PHP
 return [
-	'ELECTRUM_PATH'=>'/usr/bin/electrum'
+	'ELECTRUM_PATH'=>'/usr/local/bin/electrum'
 	,'ELECTRUM_WALLET_PATH'=>'/root/.electrum/wallets/default_wallet'
 	,'ELECTRUM_USES_SUDO'=>true
 ];
